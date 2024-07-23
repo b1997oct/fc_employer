6be561ca185ad11ed373b3265dd5eeeea40f3bbe
Bharat <b1997oct@gmail.com>
2024-07-23 21:42:53 +0530

    added login

--- app/server/login.js
+++ app/server/login.js
@@ -1,11 +1,11 @@
-import Recruiter from "@/schema/Recruiter"
 import Team from "@/schema/Team"
 import { cookies } from "next/headers"
+import { error } from "."
 
 async function login({ email, password }) {
-    let data = await Team.findOne({ email })
+    let data = await Team.findOne({ email, password })
     if (!data) {
-        return Response.json({ message: 'user not found' }, { status: 404 })
+        return error('user not found', 404)
     }
     let cookie = cookies()
     cookie.set('_tok', JSON.stringify({ user: data._id.toString(), role: 'team' }), { maxAge: 60 * 60 * 20 })
